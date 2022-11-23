da639633c4c3031091fb993981cf43eef5368fdd
Zsiga7777 <zsiganp@gmail.com>
2022-11-23 07:50:06 +0100

    orai11.23

--- 05 - Elágazások (if-else)/feladat 15/main.py
+++ 05 - Elágazások (if-else)/feladat 15/main.py
@@ -1,3 +1,5 @@
+from os import system
+
 zoldsegleves:bool=False
 husleves:bool=False
 gyumolcsleves:bool=False
@@ -18,8 +20,11 @@ kola:bool=False
 eloetel:int=None
 foetel:int=None
 koret:int=None
-valasztottMenu:str=None
 ertekeles:str = None
+
+leves:str=None
+masodik:str=None
+koretes:str=None
 
 print("Előétel:\nZöldségleves, Húsleves, Gyümölcsleves")
 print("Kérek egy kiválasztott előételt:",end='')
@@ -27,10 +32,13 @@ eloetel=int(input())
 
 if (eloetel == 1):
     zoldsegleves= True
+    leves:str="zöldség leves"
 elif (eloetel == 2):
     husleves= True
+    leves:str="húsleves"
 elif (eloetel == 3):
     gyumolcsleves= True
+    leves:str="gyümölcsleves"
 else:
     print("Ilyen nincs")
 
@@ -40,14 +48,19 @@ foetel=int(input())
 
 if (foetel == 1):
     sultcsirkecomb= True
+    masodik:str= "sültcsirkecomb" 
 elif (foetel == 2):
     sultCsikemell= True
+    masodik:str= "sült csirkemell" 
 elif (foetel == 3):
     rakottZoldseg= True
+    masodik:str= "rakottzöldség" 
 elif (foetel == 4):
     spagetti= True
+    masodik:str= "spagetti" 
 elif (foetel == 5):
     pizza= True
+    masodik:str= "pizza" 
 else:
     print("Ilyen nincs")
 
@@ -57,44 +70,41 @@ koret=int(input())
 
 if (koret == 1):
     rizs= True
+    koretes:str= "rizs" 
 elif (koret == 2):
     paroltzoldseg= True
+    koretes:str= "pároltzöldség" 
 elif (koret == 3):
     gyumolcs= True
+    koretes:str= "gyümölcs" 
 elif (koret == 4):
     sultkrumpli= True
+    koretes:str= "sültkrumpli" 
 elif (koret == 5):
     salata= True
+    koretes:str= "saláta" 
 elif (koret == 6):
     kola= True
+    koretes:str= "kóla" 
 else:
     print("Ilyen nincs")
 
 
 if((eloetel >0 and eloetel <4) and (foetel >0 and foetel <6) and (koret >0 and koret<7)):
-    if(zoldsegleves == True and spagetti == True and (gyumolcs == True or salata == True) and pizza!=True and rakottZoldseg !=True):
+    if(zoldsegleves and spagetti and (gyumolcs or salata) and pizza!=True and rakottZoldseg !=True):
         ertekeles ="Kiváló"
-        if(gyumolcs==True and salata == True):
-            valasztottMenu ="A választott menü: zöldségleves, spagetti, gyümölcs és saláta"
-        elif(gyumolcs==True):
-            valasztottMenu ="A választott menü: zöldségleves, spagetti, gyümölcs."
-        else:
-            valasztottMenu ="A választott menü: zöldségleves, spagetti, saláta"
-    elif(zoldsegleves == True and sultCsikemell == True and rizs == True and sultkrumpli!=True):
+    elif(zoldsegleves and sultCsikemell and rizs and sultkrumpli!=True):
         ertekeles ="fitnesz menü"
-        valasztottMenu ="A választott menü: zöldségleves, sült csirkemell, rizs"     
-    elif(husleves == True and sultcsirkecomb == True and salata == True and sultkrumpli==True and rakottZoldseg != True and pizza !=True):
+    elif(husleves  and sultcsirkecomb  and salata  or sultkrumpli and rakottZoldseg != True and pizza !=True):
         ertekeles ="vasárnapi menü"
-        valasztottMenu ="A választott menü: húsleves, sült csirkcomb, saláta és sültkrumpli"    
-    elif (spagetti == True or pizza==True and gyumolcs == True and kola==True  and rakottZoldseg !=True and paroltzoldseg!=True):
+    elif (spagetti  or pizza and gyumolcs  or kola  and rakottZoldseg !=True and paroltzoldseg!=True):
         ertekeles ="napimenü"
-        if(pizza==True and spagetti == True):
-            valasztottMenu ="A választott menü: pizza, spagetti, gyümölcs és kóla"
-        elif(pizza==True):
-            valasztottMenu ="A választott menü: pizza, gyümölcs és kóla."
-        else:
-            valasztottMenu ="A választott menü: spagetti, gyümölcs és kóla"
 else:
     print("nem teljes a rendelés")
 
-print(f"A választott menü értékelése:{ertekeles} \n A választott menü: {valasztottMenu}")
+if (ertekeles ==None):
+    ertekeles:str= "Egyedi"
+
+system('cls')
+
+print(f"A választott menü értékelése:{ertekeles} \n A választott menü: {leves}, {masodik}, {koretes}")
