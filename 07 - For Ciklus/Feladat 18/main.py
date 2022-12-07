f484d21d9b0a19a43c894e240a0e8365f275c5e5
Zsiga7777 <zsiganp@gmail.com>
2022-12-07 09:07:29 +0100

    vegso12.07

---
+++ 07 - For Ciklus/Feladat 18/main.py
@@ -0,0 +1,30 @@
+from os import system
+
+kezdo:int=None
+vegso:int=None
+darab:int=1
+osszeg:int=0
+
+print("Kérem a kezdő számot:", end='')
+kezdo=int(input().strip())
+
+print("Kérem a végső számot:", end='')
+vegso=int(input().strip())
+
+if (kezdo<vegso):
+    for i in range(kezdo, vegso+1):
+        if(darab % 2==0):
+            osszeg = osszeg-i
+            darab +=1
+        else:
+            osszeg = osszeg+ i
+            darab +=1
+else:
+    for i in range(vegso, kezdo+1):
+        if(darab % 2==0):
+            osszeg = osszeg-i
+            darab +=1
+        else:
+            osszeg = osszeg+ i
+            darab +=1
+print(f"{osszeg}")
