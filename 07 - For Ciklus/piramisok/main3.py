a285c19c91a5d75e032d66e72199e8d1f2348121
Zsiga7777 <zsiganp@gmail.com>
2022-12-16 07:51:10 +0100

    orai 12.16

--- 07 - For Ciklus/piramisok/main3.py
+++ 07 - For Ciklus/piramisok/main3.py
@@ -1,10 +1,15 @@
 max:int=None
 lista:list =[]
+lista2:list =[]
 
 print("Kérek egy számot:",end ='')
 max = int(input().strip())
-lista.append(\t)
 
-for i in range(1, max+1, 1):
-    lista.append(i) 
-    print("\t".join(map(str,lista)))
+for i in range(1, max*2, 1):
+    lista.append("  ") 
+
+for i in range(1, max*2, 1):
+    lista.append(i)
+    lista.append("   ")  
+    del lista[0]
+    print("".join(map(str,lista)))
