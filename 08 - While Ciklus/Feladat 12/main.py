ebb161b878ab34de3c86d6404a7ca81c5bd56f2d
Zsiga7777 <zsiganp@gmail.com>
2023-02-02 12:46:10 +0100

    házi 02.02

---
+++ 08 - While Ciklus/Feladat 12/main.py
@@ -0,0 +1,19 @@
+megtakaritas:str = None
+megtakaritasMasolat:str = None
+honapokSzama:int=0
+
+while (megtakaritas == None or megtakaritas < 0 or megtakaritas > 100000):
+    print("Kérek egy megtakarított összeget:", end='')
+    megtakaritas = input()
+    megtakaritasMasolat = megtakaritas.replace(".","").replace("-","")
+    if(megtakaritasMasolat.isnumeric()):
+        megtakaritas = float(megtakaritas)
+    else:
+        megtakaritas = None
+
+
+while (megtakaritas < 100000):
+    megtakaritas = megtakaritas * 1.02
+    honapokSzama +=1
+
+print(f"{honapokSzama} hónap alatt lesz 100000 Ft a megtakarítás")
