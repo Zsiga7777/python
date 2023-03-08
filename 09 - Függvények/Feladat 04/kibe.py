eafadfa22976537a5e8c0e8b99ab714faf58eb36
Zsiga7777 <zsiganp@gmail.com>
2023-03-08 09:35:18 +0100

    Házi 03.08

--- 09 - Függvények/Feladat 04/kibe.py
+++ 09 - Függvények/Feladat 04/kibe.py
@@ -1,18 +1,22 @@
+from colored import *
 def bekeres()->str:
     nev:str=None
     temp:str = None
+    tempMasolat:str = None
     isnev: bool = False
 
     while ((nev == None) or isnev== False or len(nev) < 2):
         print("Kérek egy nevet:", end='')
-        temp = input()
-        isnev = temp.isalpha()
+        temp = input().strip()
+        tempMasolat = temp.replace(" ", "")
+        isnev = tempMasolat.isalpha()
 
         if (isnev):
-            nev = (temp)
+            nev = (temp).title()
         else:
             print("Nem nevet adott meg.")
     return nev
 
 def kiiras(a:str)->None:
-    print(f"Üdvözlöm {a}!")
+    szam:int=len(a)
+    print (f'%s Üdvözlöm {a} %s' % (fg(szam), attr(0)))
