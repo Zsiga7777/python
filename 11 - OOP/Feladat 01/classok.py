58e77503d49ec3957459f82a8e1e485667e4159e
Zsiga7777 <zsiganp@gmail.com>
2023-04-29 10:52:20 +0200

    házi 04.29

--- 11 - OOP/Feladat 01/classok.py
+++ 11 - OOP/Feladat 01/classok.py
@@ -9,3 +9,20 @@ class Negyzet:
     def kerulet(self) -> float:
         return 4 * self.oldal
 
+
+class Telefon:
+    def __init__(self):
+        super().__init__()
+        self.gyarto: str = "Huawei"
+        self.modell: str = "Mate 20 lite"
+        self.megjelenes: int = 2018
+        self.akkumulator: int = 3750
+        self.kijelzo: str = "IPS LCD"
+        self.kijelzoMeret: float = 6.3
+        self.processzor: str = "Kirin 710"
+        self.memoria: int = 4
+        self.tarhely: int = 64
+    def kiiras(self,)->None:
+        print(f"Gyarto:{self.gyarto}\nModell:{self.modell}\nMegjelenes:{self.megjelenes}\nAkkumulátor:{self.akkumulator} mAh\nKijelző:{self.kijelzo}\nKijelző Mérete:{self.kijelzoMeret} inch\nProcesszor:{self.processzor}\nMemória mérete:{self.memoria} GB\nTárhely mérete:{self.tarhely} GB")
+
+
--- 11 - OOP/Feladat 01/main.py
+++ 11 - OOP/Feladat 01/main.py
@@ -1,5 +1,7 @@
 from classok import *
 
 n:Negyzet=Negyzet(100)
+t:Telefon=Telefon()
+t.kiiras()
 
 print(f"oldal={n.oldal}, kerület={n.kerulet()}, terület={n.terulet()}")
