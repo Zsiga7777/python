af13f8280fd7d6da60d30d8f477af94ee6b5beb2
Zsiga7777 <zsiganp@gmail.com>
2023-05-25 14:01:33 +0200

    házi05.25

--- 12 - Szöveges állományok feldolgozása/Feladat 02/functions.py
+++ 12 - Szöveges állományok feldolgozása/Feladat 02/functions.py
@@ -7,3 +7,10 @@ def temaKereses(konyvek:List[Konyv])->List[Konyv]:
         if(konyv.tema == "informatika"):
             infosKonyvek.append(konyv)
     return infosKonyvek
+
+def evjaratSzamitas(konyvek:List[Konyv])->List[Konyv]:
+    megfeleloEvjarat:List[Konyv]=[]
+    for konyv in konyvek:
+        if(int(konyv.kiadvasiEv) >=1900 and int(konyv.kiadvasiEv) <2000):
+            megfeleloEvjarat.append(konyv)
+    return megfeleloEvjarat
--- 12 - Szöveges állományok feldolgozása/Feladat 02/main.py
+++ 12 - Szöveges állományok feldolgozása/Feladat 02/main.py
@@ -12,7 +12,11 @@ infosKonyvek:List[Konyv]=temaKereses(konyvek)
 fajlKiiras(infosKonyvek,"informatika.txt")
 
 #Az 1900.txt állományba mentsük el azokat a könyveket amelyek az 1900-as években íródtak
+megfeleloSzazadbanMegjelentKonyvek:List[Konyv]=evjaratSzamitas(konyvek)
+fajlKiiras(megfeleloSzazadbanMegjelentKonyvek, "1900.txt")
 #Rendezzük az adatokat a könyvek oldalainak száma szerint csökkenő sorrendbe és a sorbarakott.txt állományba mentsük el.
+
+fajlKiiras(sorted(konyvek, key=lambda x: x.oldalszam, reverse=True),"sorbarakott.txt")
 # „kategoriak.txt” állományba mentse el a könyveket téma szerint. Például:
 # Thriller:
 # 	- könnyv1
